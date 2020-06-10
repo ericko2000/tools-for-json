4186c63700e166f4d553a4345e7dc740ce99ceb7
Eric Ko <ericko2000@hotmail.com>
2020-06-10 14:59:45 +0800

    update with description

--- show_json_01.py
+++ show_json_01.py
@@ -3,20 +3,20 @@
 import json
 
 
-def breakdown(data, level=0):
+def breakdown(json_data, level=0):  # A function to unfold json data # json_data - an object of json.load
     position1 = ' ' * 3 * level
-    if type(data) != list and type(data) != dict:
-        print('\n', position1 + str(data) + ',', end='')
-    elif type(data) == list:
-        print('\n', position1, 'level ', level + 1, type(data))
+    if type(json_data) != list and type(json_data) != dict:
+        print('\n', position1 + str(json_data) + ',', end='')
+    elif type(json_data) == list:
+        print('\n', position1, 'level ', level + 1, type(json_data))
         print(position1 + '[', end='')
-        for x in data:
+        for x in json_data:
             breakdown(x, level + 1)
         print('\n', position1 + ']')
-    elif type(data) == dict:
-        print('\n', position1, 'level ', level + 1, type(data))
+    elif type(json_data) == dict:
+        print('\n', position1, 'level ', level + 1, type(json_data))
         print(position1 + '{')
-        for x in data.items():
+        for x in json_data.items():
             print(position1 + x[0] + ': ', end="")
             if type(x[1]) != list and type(x[1]) != dict:
                 print(str(x[1]) + ',')
@@ -24,8 +24,9 @@ def breakdown(data, level=0):
                 breakdown(x[1], level + 1)
         print(position1 + '}', end='')
 
+
 filename = 'json_data_01.json'
-with open(filename, encoding='UTF-8-sig') as file:
+with open(filename, encoding='UTF-8-sig') as file:  # open a json file
     data = json.load(file)
 
-breakdown(data)
+breakdown(data)  # call function breakdown()
